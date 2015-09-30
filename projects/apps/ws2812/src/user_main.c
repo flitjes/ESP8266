5d6c1b59d8ef682480e091dfdfcfd94d2c3df211
flitjesdev <flitjesdev@gmail.com>
2015-09-30 18:59:58 +0200

    Updated with the use of ws2812

--- projects/apps/ws2812/src/user_main.c
+++ projects/apps/ws2812/src/user_main.c
@@ -3,8 +3,28 @@
 #include "osapi.h"
 #include "user_interface.h"
 #include "driver/uart.h"
+#include "ws2812.h"
 static ETSTimer tickTimer;
+#define LEDS 8
+char buffer1[LEDS*3] = { 0xFF, 0xFF, 0xFF,
+			 0xFF, 0xFF, 0xFF,
+			 0xFF, 0xFF, 0xFF,
+			 0xFF, 0xFF, 0xFF,
+			 0xFF, 0xFF, 0xFF,
+			 0xFF, 0xFF, 0xFF,
+			 0xFF, 0xFF, 0xFF,
+			 0xFF, 0xFF, 0xFF };
 
+char buffer2[LEDS*3] = { 0xFF, 0x00, 0x00,
+			 0xFF, 0x00, 0x00,
+			 0xFF, 0x00, 0x00,
+			 0xFF, 0x00, 0x00,
+			 0xFF, 0x00, 0x00,
+			 0xFF, 0x00, 0x00,
+			 0xFF, 0x00, 0x00,
+			 0xFF, 0x00, 0x00 };
+
+uint8_t toggle = 0;
 void ReceiveUART(char * val) {
 	os_printf("Received stuff: %s \r\n", val);
 }
@@ -15,7 +35,14 @@ void tickCb() {
 	os_intr_lock();
 
 	os_printf("Tick\r\n");
-
+	if(toggle == 0){
+		WS2812OutBuffer(buffer1, sizeof(buffer1), 100);
+		toggle = 1;
+	} 
+	if(toggle == 1){
+		WS2812OutBuffer(buffer2, sizeof(buffer2), 100);
+		toggle = 0;
+	} 
 	os_intr_unlock();
 	ets_wdt_enable();
 	os_timer_arm(&tickTimer, 1000, 0);
