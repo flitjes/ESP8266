9bb54b247a78d8e15b5e750c74f4ffa23a44113a
flitjes <flitjesdev@gmail.com>
2015-10-20 22:26:47 +0200

    Added udp recieving, doesn't work yet. When using lwip add define to makefile and longcalls

--- projects/apps/ws2812/src/user_main.c
+++ projects/apps/ws2812/src/user_main.c
@@ -5,6 +5,35 @@
 #include "driver/uart.h"
 #include "ws2812.h"
 
+#include "lwipopts.h"
+#include "lwip/sockets.h"
+#include "lwip/ip_addr.h"
+#include "lwip/init.h"
+#include "lwip/netif.h"
+#include "lwip/igmp.h"
+#include "lwip/udp.h"   
+
+void ICACHE_FLASH_ATTR handle_udp_recv(void *arg, struct udp_pcb *pcb, struct pbuf *p,  ip_addr_t *addr, u16_t port) {
+    int length = p->len;
+    char * pusrdata = p->payload;
+    os_printf("Received udp data: %s \r\n", pusrdata);
+}
+
+void init_udp() {
+    struct ip_addr ipSend;
+    lwip_init();
+    struct udp_pcb * pUdpConnection = udp_new();
+    IP4_ADDR(&ipSend, 255, 255, 255, 255);
+    pUdpConnection->multicast_ip = ipSend;
+    pUdpConnection->remote_ip = ipSend;
+    pUdpConnection->remote_port = 8080;
+    if(pUdpConnection == NULL) {
+        os_printf("\nCould not create new udp socket... \n");
+    }
+    int err = udp_bind(pUdpConnection, IP_ADDR_ANY, 8080);
+    udp_recv(pUdpConnection, handle_udp_recv, pUdpConnection);
+}
+
 static ETSTimer tickTimer;
 #define LEDS 20
 char buffer1[LEDS*3] = { 0x00, 0x00, 0xFF,
@@ -87,7 +116,7 @@ void setap(char * ssid) {
     apconf.ssid_hidden = 0;
     apconf.ssid_len = os_strlen(ssid);
     apconf.channel = 1;
-    
+
     wifi_softap_set_config(&apconf);
 }
 
@@ -95,7 +124,7 @@ void user_init(void) {
 	uart_init(BIT_RATE_115200, BIT_RATE_115200, ReceiveUART); // baudrate, callback, eolchar, printftouart
 	os_printf("Starting \r\n");
     setap("LedClock");
-
+    init_udp();
 	os_timer_disarm(&tickTimer);
 	os_timer_setfn(&tickTimer, tickCb, NULL);
 	os_timer_arm(&tickTimer, 1000, 0);
